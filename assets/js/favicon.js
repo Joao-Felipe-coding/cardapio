410533d34138ea2bfba3d886028a1f36c56ed754
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-03-19 20:20:55 -0300

    correção de bug relacionado ao favicon

--- assets/js/favicon.js
+++ assets/js/favicon.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const favicon = document.querySelector('link[rel="icon"]');
-    const darkModeMedia = window.matchMedia('(prefers-color-scheme: dark)');
+  const favicon = document.querySelector('link[rel="icon"]');
+  const darkModeMedia = window.matchMedia('(prefers-color-scheme: dark)');
 
-    const setFavicon = mode => {
-        favicon.href = mode.matches ? 'assets/images/site-logo/icon.svg' : 'assets/images/site-logo/icon-black.svg';
-    };
+  const setFavicon = mode => {
+    favicon.href = mode.matches ? 'assets/images/site-logo/icon.svg' : 'assets/images/site-logo/icon-black.svg';
+  };
 
-    setFavicon(darkModeMedia);
-    darkModeMedia.addEventListener('change', setFavicon);
+  setFavicon(darkModeMedia);
+  darkModeMedia.addEventListener('change', setFavicon);
 });
