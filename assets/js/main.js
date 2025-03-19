ee4d083d2f9afa501bc5f35e0eceb4e159d142e9
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-03-18 22:17:06 -0300

    adição da section pedidos, para que os pedidos possam ser adicionados em um carrinho

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ const totalElemento = document.getElementById('total');
 
 let total = 0;
 
-// Delegação de evento para capturar cliques em botões "adicionar" que podem ser criados dinamicamente
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('adicionar')) {
     const botao = event.target;
@@ -22,6 +21,7 @@ document.addEventListener('click', (event) => {
 const botaoFinalizarPedido = document.getElementById('finalizar-pedido');
 
 botaoFinalizarPedido.addEventListener("click", () => {
+  // biome-ignore lint/style/useTemplate: <explanation>
   alert("Pedido finalizado" + totalElemento.textContent);
 
   listaPedidos.innerHTML = '';
