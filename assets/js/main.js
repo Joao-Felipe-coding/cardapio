7455275392468c0c8c4df2beb8e0b2b9ea6b1995
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-03-21 11:20:58 -0300

    versão final, com algumas alterações para melhor desemprenho e montagem de código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
-// Polyfills
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-// CSS
-import './assets/css/style.css';
-import '../src/assets/css/back-to-top.css';
-import '../src/assets/css/categorias-produtos.css';
-import '../src/assets/css/mobile-nav-bar.css';
-
-// JS
-import '../src/assets/js/backToTop';
-import '../src/assets/js/mobileNavbar';
-import '../src/assets/js/pedidos';
-
 // Aguarda o carregamento completo da página e remove a tela de loading após 2 segundos adicionais
 window.addEventListener('load', () => {
   setTimeout(() => {
